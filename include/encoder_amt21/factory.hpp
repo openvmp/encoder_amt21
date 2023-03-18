da7406c6802c2b79470018294b4b2a608578797b
Roman Kuzmenko <clairbee@guidemaze.com>
2023-03-18 11:54:06 -0700

    packaged the driver as a library to be loaded by openvmp_hardware_manager

---
+++ include/encoder_amt21/factory.hpp
@@ -0,0 +1,27 @@
+/*
+ * OpenVMP, 2023
+ *
+ * Author: Roman Kuzmenko
+ * Created: 2023-03-18
+ *
+ * Licensed under Apache License, Version 2.0.
+ */
+
+#ifndef OPENVMP_REMOTE_ENCODER_AMT21_FACTORY_H
+#define OPENVMP_REMOTE_ENCODER_AMT21_FACTORY_H
+
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "remote_encoder/interface.hpp"
+
+namespace encoder_amt21 {
+
+class Factory {
+ public:
+  static std::shared_ptr<remote_encoder::Interface> New(rclcpp::Node *node);
+};
+
+}  // namespace encoder_amt21
+
+#endif  // OPENVMP_REMOTE_ENCODER_AMT21_FACTORY_H
---
+++ src/factory.cpp
@@ -0,0 +1,39 @@
+/*
+ * OpenVMP, 2023
+ *
+ * Author: Roman Kuzmenko
+ * Created: 2023-03-18
+ *
+ * Licensed under Apache License, Version 2.0.
+ */
+
+#include "encoder_amt21/factory.hpp"
+
+#include <exception>
+
+#include "encoder_amt21/interface.hpp"
+#include "remote_encoder/interface_remote.hpp"
+
+namespace encoder_amt21 {
+
+std::shared_ptr<remote_encoder::Interface> Factory::New(rclcpp::Node *node) {
+  rclcpp::Parameter use_remote;
+  if (!node->has_parameter("use_remote")) {
+    node->declare_parameter("use_remote", true);
+  }
+  node->get_parameter("use_remote", use_remote);
+
+  rclcpp::Parameter is_remote;
+  if (!node->has_parameter("encoder_is_remote")) {
+    node->declare_parameter("encoder_is_remote", use_remote.as_bool());
+  }
+  node->get_parameter("encoder_is_remote", is_remote);
+
+  if (is_remote.as_bool()) {
+    return std::make_shared<remote_encoder::RemoteInterface>(node);
+  } else {
+    return std::make_shared<Interface>(node);
+  }
+}
+
+}  // namespace encoder_amt21
